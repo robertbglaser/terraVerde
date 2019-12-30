eb636f5dacc55d501b91efdd520a00a070626270
Robert Glaser <robertbglaser@gmail.com>
2019-12-30 09:47:48 -0800

    add Barchart component

---
+++ verde/src/BarChart.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'; 
+
+
+class BarChart extends Component {
+  
+  
+
+    render() {
+
+
+    
+
+
+      
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BarChart
